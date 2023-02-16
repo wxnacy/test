5d0a3c2d57c1d88b178a612f68716eb12d149d5b
wxnacy <371032668@qq.com>
2023-02-16 13:09:45 +0800

    feat(test): 增加测试文件

---
+++ gitlint_regex.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:
+# Description:
+
+
+import re
+
+
+re.compile("")
+
+
+class A:
+    def generate_final_ops(
+        self,
+        a: int,
+        b: str,
+        name_str_name_str_name_str_name_str_name_str_name_str_: str,
+    ):
+        pass
+
+
+def test(a: int, b: str, name_str_name_str_name_str_name_str_name_str_name_str_: str):
+    pass
+print(1)
+print(1)
+print(1)
